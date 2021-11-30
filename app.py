bb655db8ac19bc15dd5767e25d6bb30dff9d0807
mysmyst <57454754+mysmyst@users.noreply.github.com>
2021-11-30 21:43:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,136 +1,135 @@
-# Packages
-from bokeh.models import ColumnDataSource, HoverTool
-from bokeh.plotting import figure, show
+from streamlit_bokeh_events import streamlit_bokeh_events
 from bokeh.models import ColumnDataSource, CustomJS
 from bokeh.plotting import figure
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-import random
-import pandas as pd
-from scipy.stats import norm
-from faker import Faker
 import numpy as np
-import plotly.express as px
-from datetime import datetime
-from datetime import timedelta
 
-#import PyQt5
-
-st.title('🧊Data Tweaking Labs_')
+st.title('Data Tweaking Labs🧊')
 st.text('Synthic Data Generation')
-st.file_uploader('Upload Data')
+dataset=st.file_uploader('Upload Data')
 
-st.radio('Pick one', ['Bias Detection and Mitigation',
+
+
+if dataset is not None:
+    datatbl=pd.read_csv(dataset)
+    st.write("Uploaded dataset:")   
+    st.dataframe(datatbl.head())
+    
+page=st.selectbox('Pick one', ['Bias Detection and Mitigation',
                       'New Data', 'More Data'])
 
+if page=='Bias Detection and Mitigation':
+    st.write("detecting bias")
+elif page=='New Data':
+    
+    ncols=st.text_area(label="Enter no. of cols",value="0")
+    
+    st.write("Describe the columns:")
+    
+    for i in range(int(ncols)):
+        st.text_area(label="col"+f"{i}",key="col"+f"{i}",height=10)
+    st.multiselect('Alogrithms', ['milk', 'apples', 'potatoes'])
 
-st.text('New Data')
+    # Here
 
-# xmin = float(input("Enter lowest x value "))
-# xmax = float(input("Enter highest x value "))
-# ymin = float(input("Enter lowest y value "))
-# ymax = float(input("Enter highest y value "))
-xmin = 1
-xmax = 5
-ymin = 5
-ymax = 50
+    df = pd.DataFrame({
+        'first column': [1, 2, 3, 4],
+        'second column': [10, 20, 30, 40]
+    })
 
+    st.dataframe(df)
 
-class LineBuilder:
-    def __init__(self, line):
-        self.line = line
-        self.xs = list(line.get_xdata())
-        self.ys = list(line.get_ydata())
-        self.cid = line.figure.canvas.mpl_connect('button_press_event', self)
+    dataframe = pd.DataFrame(
+        np.random.randn(10, 20),
+        columns=('col %d' % i for i in range(20)))
 
-    def __call__(self, event):
-        print('click', event)
-        if event.inaxes != self.line.axes:
-            return
-        self.xs.append(event.xdata)
-        self.ys.append(event.ydata)
-        self.line.set_data(self.xs, self.ys)
-        self.line.figure.canvas.draw()
+    st.dataframe(dataframe.style.highlight_max(axis=0))
+    x = st.slider('x')  # 👈 this is a widget
+    st.write(x, 'squared is', x * x)
 
-
-fig, ax = plt.subplots()
-ax.set_title('click to build line segments')
-ax.set_xlim([xmin, xmax])
-ax.set_ylim([ymin, ymax])
-line, = ax.plot([], [])  # empty line
-linebuilder = LineBuilder(line)
-st.write(fig)
-
-# fig, ax = plt.subplots()
-# ax.set_title('click to build line segments')
-# ax.set_xlim([xmin, xmax])
-# ax.set_ylim([ymin, ymax])
-# line, = ax.plot([], [])  # empty line
-# linebuilder = LineBuilder(line)
-
-# st.plotly_chart(plt.show(), filename="test")
-
-# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
-# fake = Faker()
-# data_normal = norm.rvs(size=10, loc=0, scale=1)
-# cols = []
-# calcsteps = []
-# nrows = 10
-# ncols = 5
-
-# for i in range(ncols):
-#     cols.append(list(range(nrows)))
-#     calcsteps.append([])
+    st.text_input("hi", key="hi")
 
 
-# def poshness(inp1, inp2):
-#     if((inp2-(18500+((inp1-2)*5000))) > 0):
-#         return 'prime'
-#     else:
-#         return 'regular'
+    st.line_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+    with st.expander("See explanation"):
+        st.write("""
+            The chart above shows some numbers I picked for you.
+            I rolled actual dice for these, so they're *guaranteed* to
+            be random.
+        """)
+        st.image("https://emoji.gg/assets/emoji/1721_cozysip.png")
 
 
-# def locality(poshness):
-#     ls = ['Mumbai', 'Pune', 'Mahableshwar', 'Goa']
-#     if (poshness == 'prime'):
-#         locality = ls[random.randint(0, 1)]
-#     else:
-#         locality = ls[random.randint(2, 3)]
-#     return locality
+    # Add a selectbox to the sidebar:
+    add_selectbox = st.sidebar.selectbox(
+        'How would you like to be contacted?',
+        ('Email', 'Home phone', 'Mobile phone')
+    )
+
+    # Add a slider to the sidebar:
+    add_slider = st.sidebar.slider(
+        'Select a range of values',
+        0.0, 100.0, (25.0, 75.0)
+    )
+
+    left_column, right_column = st.columns(2)
+    # You can use a column just like st.sidebar:
+    left_column.button('Press me!')
+
+    # Or even better, call Streamlit functions inside a "with" block:
+    with right_column:
+        chosen = st.radio(
+            'Sorting hat',
+            ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+        st.write(f"You are in {chosen} house!")
 
 
-# def propage(cpr, price, poshness):
-#     mp = max(price)
-#     mnp = min(price)
-#     pd = (mp+mnp)/2
-#     if (poshness == 'prime'):
-#         pm = 1.5
-#     else:
-#         pm = 1
-#     return abs((random.randint(1, 5)+(pd/cpr)*pm))
+    # import function
 
+    # create plot
+    p = figure(tools="lasso_select")
+    cds = ColumnDataSource(
+        data={
+            "x": [1, 2, 3, 4],
+            "y": [4, 5, 6, 7],
+        }
+    )
+    p.circle("x", "y", source=cds)
 
-# # print(cols)
-# # print(calcsteps)
-# # # random.randint(10000,20000)+((cols[i-1][j]-2)*5000)
-# # print(len(houseprices))
-# calcsteps[0].append('random.randint(1,4)')
-# calcsteps[1].append('random.randint(14000,20000)+((cols[i-1][j]-2)*5000)')
-# calcsteps[2].append('poshness(cols[i-2][j],cols[i-1][j])')
-# calcsteps[3].append('locality(cols[i-1][j])')
-# calcsteps[4].append('propage(cols[i-3][j],cols[i-3],cols[i-2][j])')
+    # define events
+    cds.selected.js_on_change(
+        "indices",
+        CustomJS(
+            args=dict(source=cds),
+            code="""
+            document.dispatchEvent(
+                new CustomEvent("YOUR_EVENT_NAME", {detail: {your_data: "goes-here"}})
+            )
+            """
+        )
+    )
 
-# for i in range(ncols):
-#     for j in range(nrows):
-#         for k in range(len(calcsteps[i])):
-#             cols[i][j] = eval(calcsteps[i][k])
+    # result will be a dict of {event_name: event.detail}
+    # events by default is "", in case of more than one events pass it as a comma separated values
+    # event1,event2
+    # debounce is in ms
+    # refresh_on_update should be set to False only if we dont want to update datasource at runtime
+    # override_height overrides the viewport height
+    result = streamlit_bokeh_events(
+        bokeh_plot=p,
+        events="YOUR_EVENT_NAME",
+        key="foo",
+        refresh_on_update=False,
+        override_height=600,
+        debounce_time=500)
 
-
-# x = pd.DataFrame(cols)
-# x_n = x.rename(index={0: 'bhk', 1: 'price', 2: 'locality',
-#                       3: 'location', 4: 'age in years'})
-# x = x_n.transpose()
-# st.write(x.head(10))
+    # use the result
+    st.write(result)
+elif page=='More Data':
+    st.write("more data")
+else:
+    pass
