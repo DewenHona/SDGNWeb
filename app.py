84759a6d238cf3fab08efbe783e9d29dad43038c
DewenHona <45680052+DewenHona@users.noreply.github.com>
2021-12-03 01:39:50 +0530

    Added Basic Features
    Co-authored-by: mysmyst <mysmyst@users.noreply.github.com>

--- app.py
+++ app.py
@@ -10,123 +10,292 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import numpy as np
+import random
+from faker import Faker
 
-st.title('Data Tweaking Labs🧊')
-st.text('Synthic Data Generation')
-dataset = st.file_uploader('Upload Data')
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn import metrics
+
+from aif360.sklearn.inprocessing import GridSearchReduction
+from aif360.sklearn.datasets import fetch_adult
+from aif360.sklearn.metrics import disparate_impact_ratio, average_odds_error, generalized_fpr
+from aif360.sklearn.metrics import generalized_fnr, difference
+from aif360.algorithms.preprocessing import DisparateImpactRemover
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
+fake = Faker()
+
+
+def colip(coltype, nrows, i):
+    if coltype == "int":
+        x = int(st.text_input(label="enter an integer", value=0, key="int"+str(i)))
+        return [x]*nrows
+
+    if coltype == "string":
+        x2 = st.text_input(label="enter a string",
+                           value="foo", key="str"+str(i))
+        return [x2]*nrows
+
+    if coltype == "sequence":
+        start = int(st.text_input(
+            label="enter start value", value=0, key="sta"+str(i)))
+        increment = int(st.text_input(
+            label="enter increment value", value=1, key="inc"+str(i)))
+        op = [None]*nrows
+        for i in range(nrows):
+            op[i] = start+(i*increment)
+        return op
+
+    if coltype == "names":
+        nametype = st.selectbox(
+            "select name type", ('full name', 'first name', 'first name - male', 'first name - female', 'last name'), key="nt"+str(i))
+        op = [None]*nrows
+        if nametype == 'full name':
+            for i in range(nrows):
+                op[i] = fake.name()
+        elif nametype == 'first name':
+            for i in range(nrows):
+                op[i] = fake.first_name()
+        elif nametype == 'first name - male':
+            for i in range(nrows):
+                op[i] = fake.first_name_male()
+        elif nametype == 'first name - female':
+            for i in range(nrows):
+                op[i] = fake.first_name_female()
+        elif nametype == 'last name':
+            for i in range(nrows):
+                op[i] = fake.last_name()
+        return op
+
+    if coltype == "countries":
+        op = [None]*nrows
+        for i in range(nrows):
+            op[i] = fake.country()
+        return op
+
+    if coltype == "list":
+        lst = st.text_input(
+            label="enter a list of things, comma seperated", value="foo, bar, baz", key="lab"+str(i))
+        howlst = st.selectbox(
+            "how do you want to generate the column", ('random', 'in sequence'), key="howl"+str(i))
+        lst = lst.split(sep=",")
+        llen = len(lst)
+        if howlst == "random":
+            lop = []
+            for i in range(nrows):
+                lop.append(lst[random.randint(0, llen-1)])
+        elif howlst == "in sequence":
+            lop = []
+            for i in range(nrows):
+                lop.append(lst[i % llen])
+
+        return lop
+
+    if coltype == "distribution":
+        return 0
+
+
+# dataset = st.file_uploader('Upload Data')
 testoutput = []
+
+
+page = st.sidebar.radio('What do you want to do?', ('Home', 'Bias Detection and Mitigation',
+                                                    'New Data', 'More Data', 'Test'))
+dataset = st.sidebar.file_uploader('Upload Data')
 
 if dataset is not None:
     datatbl = pd.read_csv(dataset)
-    st.write("Uploaded dataset:")
-    st.dataframe(datatbl.head())
+    st.sidebar.write("Uploaded dataset:")
+    st.sidebar.dataframe(datatbl.head())
 
-page = st.selectbox('Pick one', ['Bias Detection and Mitigation',
-                                 'New Data', 'More Data'])
+htmlp2 = '''
+            <style>
+        .floating-menu {
+            font-family: sans-serif;
+            background: black;
+            padding: 5px;;
+            width: 180px;
+            z-index: 100;
+            position: fixed;
+            bottom: 0px;
+            right: 0px;
+        }
+        
+        .floating-menu a, 
+        .floating-menu h3 {
+            font-size: 0.9em;
+            display: block;
+            margin: 0 0.5em;
+            color: white;
+        }
+        </style>
+        <nav class="floating-menu">
+            <h3>Data Tweaking Labs🧊</h3>
+            <a href="https://github.com/synthdatagen" target="_blank">Github</a>
+            <a href="https://docs.google.com/presentation/d/1H2cDNjSosFWmXeIfXjcs_qUbVFiEZqbJBaqiaUtnAws/edit?usp=sharing" target="_blank">Presentation</a>
+            <a href="" target="_blank">Paper</a>
+        
+        </nav>
+    '''
+st.markdown(htmlp2, unsafe_allow_html=True)
+if page == 'Home':
+    htmlp1 = '''
+            <style>
+                .text {
+                    color: #444444;
+                    background: #FFFFFF;
+                    text-shadow: 1px 0px 1px #CCCCCC, 0px 1px 1px #EEEEEE, 2px 1px 1px #CCCCCC, 1px 2px 1px #EEEEEE, 3px 2px 1px #CCCCCC, 2px 3px 1px #EEEEEE, 4px 3px 1px #CCCCCC, 3px 4px 1px #EEEEEE, 5px 4px 1px #CCCCCC, 4px 5px 1px #EEEEEE, 6px 5px 1px #CCCCCC, 5px 6px 1px #EEEEEE, 7px 6px 1px #CCCCCC;
+                    color: #444444;
+                    background: #FFFFFF;
 
+                }
+                    del {
+                    background: #000;
+                    color: #fff;
+                    text-decoration:none;
+                    }
+                .bruh{
+                    display:inline;
+                    margin-right:10px;
+                }
+
+
+            </style>
+            <h1 class="text">Data Tweaking Labs🧊</h1>
+            <h3 class="text bruh"><i>Synthetic Data Generation</i></h3>
+            <del>v0.0.1</del>
+            '''
+
+    st.markdown(htmlp1, unsafe_allow_html=True)
+
+    st.write("Synthetic data, as the name suggests, is data that is artificially created rather than being generated by actual events. It is often created with the help of algorithms and is used for a wide range of activities, including as test data for new products and tools, for model validation, and in AI model training. Synthetic data is important for businesses due to three reasons: privacy, product testing and training machine learning algorithms.")
+    st.subheader('Benefits of synthetic Data:')
+    st.write('1. Overcoming real data usage restrictions: Real data may have usage constraints due to privacy rules or other regulations. Synthetic data can replicate all important statistical properties of real data without exposing real data, thereby eliminating the issue.')
+    st.write('2. Creating data to simulate not yet encountered conditions: Where real data does not exist, synthetic data is the only solution.')
+    st.write('3. Immunity to some common statistical problems: These can include item nonresponse, skip patterns, and other logical constraints.')
+    st.write('4. Immunity to some common statistical problems: These can include item nonresponse, skip patterns, and other logical constraints.')
+    st.subheader('Bias Mitigation:')
+    st.write("For Bias detection our applications utilizes already established metrics and mitigation algorithms by IBM-AIF360. Further In our work we implement those on new datasets. Also we use this tool to interpret if AI models generate bias data. If yes we provide a mitigated data")
+
+
+# Navigation of the App
 if page == 'Bias Detection and Mitigation':
-    st.write("detecting bias")
+    st.subheader("Coming Soon ⚙")
+    # BIAS DETECTION
+
+    if dataset is not None:
+
+        st.write(datatbl)
+        datatbl = datatbl.dropna(how='any', axis=0)
+        datatbl.info()
+
+        datatbl = datatbl.dropna(how='any', axis=0)
+        datatbl.info()
+
+        # Drop unnecessary column
+        datatbl = datatbl.drop(['Loan_ID'], axis=1)
+
+
+# NEW DATA
 elif page == 'New Data':
 
-    ncols = st.text_area(label="Enter no. of cols", value="0")
+    st.subheader("Create your dataset:")
 
-    st.write("Describe the columns:")
+    ncols = int(st.text_input(label="Enter no. of cols", value=1))
+    nrows = int(st.text_input(label="Enter no. of rows", value=10))
+    colnames = [None]*ncols
+    coltypes = [None]*ncols
+    tblcols = [None]*ncols
+
+    st.write("_________")
+    st.markdown("<h4>Describe the columns:</h4>",
+                unsafe_allow_html=True)
 
     for i in range(int(ncols)):
-        st.text_area(label="col"+f"{i}", key="col"+f"{i}", height=10)
-    st.multiselect('Alogrithms', ['milk', 'apples', 'potatoes'])
+        colnames[i] = st.text_input(
+            label="column "+f"{i}", key="coln"+f"{i}")
+        coltypes[i] = st.selectbox("column type for column "+f"{i}"+" ("+f"{colnames[i]}"+")", (
+            'int', 'string', 'sequence', 'names', 'countries', 'list', 'distribution'), key="colt"+f"{i}")
+        tblcols[i] = colip(coltypes[i], nrows, i)
+        st.write("____________")
 
-    # Here
+    st.subheader("no of rows:")
+    st.write(f"{nrows}")
+    st.subheader("columns added:")
+    for i in range(ncols):
+        st.write("col "+f"{i}"+" - name: " +
+                 colnames[i]+" | type: "+coltypes[i]+"\n")
+    st.write("____________")
+    st.markdown("<h3>Generated Table: </h3>", unsafe_allow_html=True)
+    df = pd.DataFrame(tblcols)
 
-    # create plot
-    # p = figure(tools="lasso_select")
-    # cds = ColumnDataSource(
-    #     data={
-    #         "x": [1, 2, 3, 4],
-    #         "y": [4, 5, 6, 7],
-    #     }
-    # )
-    # p.circle("x", "y", source=cds)
-
-    # # define events
-    # cds.selected.js_on_change(
-    #     "indices",
-    #     CustomJS(
-    #         args=dict(source=cds),
-    #         code="""
-    #         document.dispatchEvent(
-    #             new CustomEvent("YOUR_EVENT_NAME", {detail: {your_data: "goes-here"}})
-    #         )
-    #         """
-    #     )
-    # )
-
-    # # result will be a dict of {event_name: event.detail}
-    # # events by default is "", in case of more than one events pass it as a comma separated values
-    # # event1,event2
-    # # debounce is in ms
-    # # refresh_on_update should be set to False only if we dont want to update datasource at runtime
-    # # override_height overrides the viewport height
-    # result = streamlit_bokeh_events(
-    #     bokeh_plot=p,
-    #     events="YOUR_EVENT_NAME",
-    #     key="foo",
-    #     refresh_on_update=False,
-    #     override_height=600,
-    #     debounce_time=500)
-
-    # # use the result
-    # st.write(result)
-
-    xmin, xmax, ymin, ymax = 2, 4, 5, 15
-    xmin = st.slider(label="x-min", min_value=1,
-                     max_value=100, value=10, step=1)
-    xmax = st.slider(label="x-max", min_value=xmin+1,
-                     max_value=xmin+100, value=10, step=1)
-    ymin = st.slider(label="y-min", min_value=1,
-                     max_value=100, value=10, step=1)
-    ymax = st.slider(label="y-max", min_value=ymin+1,
-                     max_value=ymin+100, value=10, step=1)
-    fig, ax = plt.subplots()
-    ax.set_xlim([xmin, xmax])
-    ax.set_ylim([ymin, ymax])
-    line, = ax.plot([], [])
-    plt.savefig('img.png')
-    i = Image.open('img.png')
-    # st.image(i)
-    # st.pyplot(fig)
-
-    canvas_result = st_canvas(
-        stroke_width=1, background_image=i, update_streamlit=True)
-    testoutput.append(canvas_result)
-    print(canvas_result)
-
-    if st.button(label="print output"):
-        st.write(testoutput)
-
-    xmin2 = st.slider(label="x-min2", min_value=1,
-                      max_value=100, value=10, step=1)
-    xmax2 = st.slider(label="x-max2", min_value=xmin2+1,
-                      max_value=xmin2+100, value=20, step=1)
-    ymin2 = st.slider(label="y-min2", min_value=1,
-                      max_value=100, value=10, step=1)
-    ymax2 = st.slider(label="y-max2", min_value=ymin2+1,
-                      max_value=ymin2+100, value=20, step=1)
-
-    precision = st.slider(label="precision", min_value=5,
-                          max_value=10, value=5, step=1)
-    if precision is not None:
-        points = [0]*precision
-        for i in range(precision):
-            st.slider(min_value=ymin2, max_value=ymax2, key=str(i) +
-                      "pt", label=str((xmin2+(((xmax2-xmin2)/precision)*i+1))))
-
+    df2 = df.transpose()
+    df2.columns = colnames
+    st.table(df2.head())
 
 elif page == 'More Data':
-    st.write("more data")
-else:
-    pass
+    st.subheader("Coming Soon ⚙")
+elif page == 'Test':
 
+    def plot_and_move(df):
+        p = figure(x_range=(0, 10), y_range=(0, 10), tools=[],
+                   title='Point Draw Tool')
+
+        source = ColumnDataSource(df)
+
+        renderer = p.scatter(x='x', y='y', source=source, size=10)
+
+        draw_tool = PointDrawTool(renderers=[renderer])
+        p.add_tools(draw_tool)
+        p.toolbar.active_tap = draw_tool
+
+        source.js_on_change("data", CustomJS(
+            code="""
+                document.dispatchEvent(
+                    new CustomEvent("DATA_CHANGED", {detail: cb_obj.data})
+                )
+            """
+        ))
+
+        event_result = streamlit_bokeh_events(
+            p, key="foo", events="DATA_CHANGED", refresh_on_update=False, debounce_time=0)
+
+        if event_result:
+            df_values = event_result.get("DATA_CHANGED")
+            return pd.DataFrame(df_values, index=df_values.pop("index"))
+        else:
+            return df
+
+    df = pd.DataFrame({
+        'x': [1, 5, 9], 'y': [1, 5, 9]
+    })
+
+    st.write(plot_and_move(df))
+
+
+# Hide Hamborgor and "Made with StreamLit"
+hide_streamlit_style = """
+            <style>
+            # MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+hide_decoration_bar_style = '''
+    <style>
+        header {visibility: hidden;}
+    </style>
+'''
+st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+
+# -------------------------------------------------------------------------------------
 
 # output_file("tools_point_draw.html")
 
@@ -152,40 +321,3 @@ else:
 
 
 # show(Column(p, table))
-
-
-def plot_and_move(df):
-    p = figure(x_range=(0, 10), y_range=(0, 10), tools=[],
-               title='Point Draw Tool')
-
-    source = ColumnDataSource(df)
-
-    renderer = p.scatter(x='x', y='y', source=source, size=10)
-
-    draw_tool = PointDrawTool(renderers=[renderer])
-    p.add_tools(draw_tool)
-    p.toolbar.active_tap = draw_tool
-
-    source.js_on_change("data", CustomJS(
-        code="""
-            document.dispatchEvent(
-                new CustomEvent("DATA_CHANGED", {detail: cb_obj.data})
-            )
-        """
-    ))
-
-    event_result = streamlit_bokeh_events(
-        p, key="foo", events="DATA_CHANGED", refresh_on_update=False, debounce_time=0)
-
-    if event_result:
-        df_values = event_result.get("DATA_CHANGED")
-        return pd.DataFrame(df_values, index=df_values.pop("index"))
-    else:
-        return df
-
-
-df = pd.DataFrame({
-    'x': [1, 5, 9], 'y': [1, 5, 9]
-})
-
-st.write(plot_and_move(df))
